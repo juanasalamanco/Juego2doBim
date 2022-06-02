db12700639d2ac9667395ad6d0ae3bd8c9797ffa
juanasalamanco <juanasalamanco@gmail.com>
2022-06-02 14:34:32 -0300

    Initial commit

---
+++ Assets/DeathwallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathwallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "deathwall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/MovimientoPersonaje.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int maxJump;
+
+    int hasJump;
+    Rigidbody rb;
+
+    void Start()
+    {
+        hasJump = maxJump;
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) && hasJump == maxJump)
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D) && hasJump == maxJump)
+        {
+            transform.Translate(movementSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && hasJump>0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump--;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "ground")
+        {
+            hasJump = maxJump;
+        }
+    }
+}
