81593d2e04be4e1d5b1b7b1ae061df4be65026df
juanasalamanco <juanasalamanco@gmail.com>
2022-06-03 09:08:55 -0300

    Avance

---
+++ Assets/movobstaculoesfera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movobstaculoesfera : MonoBehaviour
+{
+    bool IsRight = true;
+    public float MovSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time;
+
+        if (time > 5) {
+
+            if (transform.position.x <= 3.75f)
+            {
+                IsRight = false;
+            }
+            else if (transform.position.x >= 3.75f)
+            {
+                IsRight = true;
+            }
+
+            if (!IsRight)
+            {
+                transform.Translate(MovSpeed, 0, 0);
+            }
+
+            if (IsRight)
+            {
+                transform.Translate(-MovSpeed, 0, 0);
+            }
+        }
+    }
+}
